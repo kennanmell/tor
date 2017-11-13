c0f4191508141e0590c9e4fdedaad9c23566dc7f
Kennan Mell <kmell96@cs.washington.edu>
2017-11-13 15:05:19 -0800

    .

--- projProxy/src/RequestThread.java
+++ projProxy/src/RequestThread.java
@@ -55,7 +55,7 @@ public class RequestThread extends Thread {
           ResponseThread newThread = new ResponseThread(clientSocket, socket);
           newThread.start();
           while (!currentHeaderLines.isEmpty()) {
-            System.out.print(currentHeaderLines.get(0)); // debug
+            //System.out.print(currentHeaderLines.get(0)); // debug
             clientSocket.getOutputStream().write(currentHeaderLines.remove(0).getBytes());
           }
         } else if (line.equalsIgnoreCase("Connection: keep-alive\n")) {
@@ -78,7 +78,7 @@ public class RequestThread extends Thread {
         if (clientSocket == null) {
           currentHeaderLines.add(line);
         } else {
-          System.out.print(line); // debug
+          //System.out.print(line); // debug
           clientSocket.getOutputStream().write(line.getBytes());
         }
       } catch (IOException e) {

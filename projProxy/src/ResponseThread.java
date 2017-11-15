b6eb2ef88321417dde57a1cbfc8f4211ae50fc04
Kennan Mell <kmell96@cs.washington.edu>
2017-11-15 14:47:48 -0800

    .

--- projProxy/src/ResponseThread.java
+++ projProxy/src/ResponseThread.java
@@ -18,26 +18,28 @@ public class ResponseThread extends Thread {
   public void run() {
     try {
       boolean parsedHeader = false;
-      String line = "";
+      StringBuilder line = new StringBuilder();
       int curr;
       while ((curr = readSocket.getInputStream().read()) != -1) {
         if (parsedHeader) {
           writeSocket.getOutputStream().write(curr);
         } else {
-          line += (char) curr;
+          line.append((char) curr);
           if (curr == (int) '\n') {
-            line = line.replace("HTTP/1.1", "HTTP/1.0");
-            if (line.trim().equalsIgnoreCase("Connection: keep-alive")) {
-              line = "Connection: close\r\n";
-            } else if (line.trim().equalsIgnoreCase("Proxy-connection: keep-alive")) {
-              line = "Proxy-connection: close\r\n";
+            String lineString = line.toString();
+            if (lineString.contains("HTTP/1.1")) {
+              line = new StringBuilder(lineString.replace("HTTP/1.1", "HTTP/1.0"));
+            } else if (lineString.trim().equalsIgnoreCase("Connection: keep-alive")) {
+              line = new StringBuilder("Connection: close\r\n");
+            } else if (lineString.trim().equalsIgnoreCase("Proxy-connection: keep-alive")) {
+              line = new StringBuilder("Proxy-connection: close\r\n");
             }
             System.out.print(line);
-            writeSocket.getOutputStream().write(line.getBytes());
-            if (line.equals("\n") || line.equals("\r\n")) {
+            writeSocket.getOutputStream().write(line.toString().getBytes());
+            if (lineString.equals("\n") || lineString.equals("\r\n")) {
               parsedHeader = true;
             }
-            line = "";
+            line = new StringBuilder();
           }
         }
       }

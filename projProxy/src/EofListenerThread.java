9751eed2151d986048af9d0fd7313f34db144b4d
Kennan Mell <kmell96@cs.washington.edu>
2017-11-15 14:56:12 -0800

    .

--- projProxy/src/EofListenerThread.java
+++ projProxy/src/EofListenerThread.java
@@ -1,6 +1,18 @@
+package src;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class EofListenerThread extends Thread {
   @Override
   public void run() {
-    
+    Scanner console = new Scanner(System.in);
+    try {
+      while (true) {
+        console.nextLine();
+      }
+    } catch (NoSuchElementException e) {
+        System.exit(0);
+    }
   }
 }

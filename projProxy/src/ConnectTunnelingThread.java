34047042b481bb08133ea5e91903d1de5d13c9e7
Kennan Mell <kmell96@cs.washington.edu>
2017-11-15 17:14:34 -0800

    comments

--- projProxy/src/ConnectTunnelingThread.java
+++ projProxy/src/ConnectTunnelingThread.java
@@ -6,30 +6,30 @@ import java.net.Socket;
 /** ConnectTunnelingThread reads data byte-by-byte from from a TCP socket and writes that data
     to another TCP socket. Intended to be used for HTTP connect tunneling. */
 public class ConnectTunnelingThread extends Thread {
-  /// The socket to read data from.
-  private Socket readSocket;
-  /// The socket to write data to.
-  private Socket writeSocket;
+  /// The socket connected to the browser to write data to.
+  private Socket clientSocket;
+  /// The socket connected to the server to read data from.
+  private Socket serverSocket;
 
   /** Sole constructor.
-      @param readSocket The TCP socket to read data from (must not be null).
-      @param writeSocket The TCP socket to write data to (must not be null). */
-  public ConnectTunnelingThread(Socket readSocket, Socket writeSocket) {
-    this.readSocket = readSocket;
-    this.writeSocket = writeSocket;
+      @param serverSocket The TCP socket to read data from (must not be null).
+      @param clientSocket The TCP socket to write data to (must not be null). */
+  public ConnectTunnelingThread(Socket clientSocket, Socket serverSocket) {
+    this.clientSocket = clientSocket;
+    this.serverSocket = serverSocket;
   }
 
   @Override
   public void run() {
     try {
       int curr;
-      while ((curr = readSocket.getInputStream().read()) != -1) {
-          writeSocket.getOutputStream().write(curr);
+      while ((curr = serverSocket.getInputStream().read()) != -1) {
+          clientSocket.getOutputStream().write(curr);
       }
     } catch (IOException e) {
       try {
-        readSocket.close();
-        writeSocket.close();
+        serverSocket.close();
+        clientSocket.close();
       } catch (IOException e2) {
         // no op
       }

78764f95ab46794b8d51614bcb68451fec34dd1c
Bryan Yue <bryanyue@MININT-RHB83P6.redmond.corp.microsoft.com>
2017-11-18 00:28:10 -0800

    added Tor Command Manager

--- projTor61/src/TorCommandManager.java
+++ projTor61/src/TorCommandManager.java
@@ -136,7 +136,7 @@ public class TorCommandManager {
     if (command != TorCommand.RELAY) {
     	return null;
     }
-    byte[] b = Arrays.asList(cell);
+    List<Byte> b = Arrays.asList(cell);
     return b.subList(14, cell.length);
   }
 }

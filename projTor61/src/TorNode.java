5311a540337e390b33e19b9aa919134f44d28285
Bryan Yue <bryanyue@MININT-RHB83P6.redmond.corp.microsoft.com>
2017-11-18 12:26:40 -0800

    added class headers

--- projTor61/src/TorNode.java
+++ projTor61/src/TorNode.java
@@ -1,3 +1,14 @@
+// Refactor HTTPRequest Thread
 public class TorNode {
-	
+	// constant for max hops (3)
+	// field: routing table
+	// field: group number
+	// field: instance number
+	// field: ConcurrentHashMap of circuit number to 
+	// field: ConcurrentHashMap of stream IDs to Concurrent Queues (passed on to RouterThread and ProxyThread)
+	// spawn 2 threads: RouterThread, ProxyThread (will assign stream IDs to each HTTPRequestThread)
+	// check that there is only one tcp connection beteween this TorNode and another
+    
+
+
 }

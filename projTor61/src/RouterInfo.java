17ec65eb05216d2a11453da70d5a319dc2ec4c40
Bryan Yue <bryanyue@MININT-RHB83P6.redmond.corp.microsoft.com>
2017-12-03 18:07:00 -0800

    started RouterThread in tor main, edited routerInfo, RouterThread

--- projTor61/src/RouterInfo.java
+++ projTor61/src/RouterInfo.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.net.Socket;
+import java.util.Map;
+import java.util.HashMap;
 
 // class to hold router information, passed on to proxy and router threads from tor main
 public class RouterInfo {
@@ -11,22 +13,20 @@ public class RouterInfo {
 	private final int instanceNumber;
 	private final int agentID;
 	private final int port;
-
 	// next available stream ID
 	private int nextStreamID;
-
 	// router ID to next available circuit ID
-	private ConcurrentMap<Integer, Integer> nextEvenCircuitID;
-	private ConcurrentMap<Integer, Integer> nextOddCircuitID;
+	private Map<Integer, Integer> nextEvenCircuitID;
+	private Map<Integer, Integer> nextOddCircuitID;
+	// router ID's to sockets
+	private Map<Integer, Socket> agentIDToSocket;
+	// routing table entries of (agentID, circuitID) tuples
+	private Map<RouterEntry, RouterEntry> routingTable;
 
 	// socket reader on router side puts cell into buffer for proxy reader to get
 	private ConcurrentMap<Integer, ConcurrentLinkedQueue<byte[]>> streamIDToBuffer;
 
-	// router ID's to sockets
-	private ConcurrentMap<Integer, Socket> routerIDToSocket;
-
-	// routing table entries of (routerID, circuitID) tuples
-	private ConcurrentMap<RouterEntry, RouterEntry> routingTable;
+	
 
     public RouterInfo(int groupNumber, int instanceNumber, int port) {
     	this.groupNumber = groupNumber;
@@ -34,11 +34,14 @@ public class RouterInfo {
     	this.agentID = (groupNumber << 16) | instanceNumber;
     	this.port = port;
     	this.nextStreamID = 1;
-    	this.nextEvenCircuitID = new ConcurrentHashMap<Integer, Integer>();
-    	this.nextOddCircuitID = new ConcurrentHashMap<Integer, Integer>();
+    	this.nextEvenCircuitID = new HashMap<Integer, Integer>();
+    	this.nextOddCircuitID = new HashMap<Integer, Integer>();
+    	this.agentIDToSocket = new HashMap<Integer, Socket>();
+    	this.routingTable = new HashMap<RouterEntry, RouterEntry>();
+
+
+
     	this.streamIDToBuffer = new ConcurrentHashMap<Integer, ConcurrentLinkedQueue<byte[]>>();
-    	this.routerIDToSocket = new ConcurrentHashMap<Integer, Socket>();
-    	this.routingTable = new ConcurrentHashMap<RouterEntry, RouterEntry>();
     }
 
     public int getGroupNumber() {
@@ -64,25 +67,72 @@ public class RouterInfo {
     	return ret;
     }
 
-    // Returns next available even circuit ID given the next routerID
-    public synchronized int getNextEvenCircuitID(int routerID) {
-    	if (!nextEvenCircuitID.containsKey(routerID) {
-    		nextEvenCircuitID.put(routerID, 2);
+    // Returns next available even circuit ID given the next agentID
+    public synchronized int getNextEvenCircuitID(int agentID) {
+    	if (!nextEvenCircuitID.containsKey(agentID) {
+    		nextEvenCircuitID.put(agentID, 2);
     	}
-    	int ret = nextEvenCircuitID.get(routerID);
-    	nextEvenCircuitID.put(routerID, ret + 2);
+    	int ret = nextEvenCircuitID.get(agentID);
+    	nextEvenCircuitID.put(agentID, ret + 2);
     	return ret;
     }
 
-    // Returns next available even circuit ID given the next routerID
-    public synchronized int getNextOddCircuitID(int routerID) {
-    	if (!nextOddCircuitID.containsKey(routerID) {
-    		nextOddCircuitID.put(routerID, 1);
+    // Returns next available odd circuit ID given the next agentID
+    public synchronized int getNextOddCircuitID(int agentID) {
+    	if (!nextOddCircuitID.containsKey(agentID) {
+    		nextOddCircuitID.put(agentID, 1);
     	}
-    	int ret = nextOddCircuitID.get(routerID);
-    	nextOddCircuitID.put(routerID, ret + 2);
+    	int ret = nextOddCircuitID.get(agentID);
+    	nextOddCircuitID.put(agentID, ret + 2);
     	return ret;
     }
+
+    // Returns the socket associated with the agentID. Returns null if there was no mapping
+    public synchronized Socket getSocket(int agentID) {
+    	return agentIDToSocket.get(agentID);
+    }
+
+    // Adds the (agentID, socket) to the map and returns the previous socket, or null if there
+    // was no mapping for the key.
+    public synchronized Socket addSocket(int agentID, Socket socket) {
+    	return agentIDToSocket.put(agentID, socket);
+    }
+
+    // Removes the agentID (and its corresponding socket) from this map. This method does nothing if the key is not in the map.
+    public synchronized Socket removeSocket(int agentID) {
+    	return agentIDToSocket.remove(agentID);
+    }
+
+    // Returns the number of connected agents.
+    public synchronized int numConnectedAgents() {
+    	return agentIDToSocket.size();
+    }
+
+    // Returns the next RouterEntry (socket, circuitID) to get to the next hop.
+    // Returns null if not found.
+    public synchronized RouterEntry getEntry(RouterEntry entry) {
+    	return routingTable.get(entry);
+    }
+
+    // Adds the previous and next RoutingEntry to the routing table. Returns null if there was no mapping for the previous routing entry.
+    public synchronized RouterEntry addEntry(RouterEntry previous, RouterEntry next) {
+    	return routingTable.put(previous, next);
+    }
+
+    // Removes the agentID (and its corresponding socket) from this map. This method does nothing if the key is not in the map.
+    public synchronized RoutingEntry removeEntry(RouterEntry previous) {
+    	return routingTable.remove(previous);
+    }
+
+    // Returns the number of connected agents.
+    public synchronized int numConnectedAgents() {
+    	return agentIDToSocket.size();
+    }
+
+
+
+
+
 
 
 }

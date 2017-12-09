ccafacd7031e4d4af0c91bcc364da24afabd65b6
Bryan Yue <bryanyue@MININT-RHB83P6.redmond.corp.microsoft.com>
2017-12-08 23:48:18 -0800

    debugging prints

--- projTor61/src/proxy/HttpRequestThread.java
+++ projTor61/src/proxy/HttpRequestThread.java
@@ -300,6 +300,7 @@ public class HttpRequestThread extends Thread {
      System.out.println("wrote begin");
 
     try {
+      System.out.println("attempting to take from buffer");
       byte[] connectedCell = responseBuf.take();//poll(25000, TimeUnit.MILLISECONDS);
       if (connectedCell != null && connectedCell[2] == 3 && connectedCell[13] == 4) {
         System.out.println("Received CONNECTED relay cell");

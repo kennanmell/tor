b34a0e5988a941f5f01a495de2d6be3152acdd0e
Bryan Yue <bryanyue@MININT-RHB83P6.redmond.corp.microsoft.com>
2017-12-08 23:01:14 -0800

    debugging prints

--- projTor61/src/proxy/HttpRequestThread.java
+++ projTor61/src/proxy/HttpRequestThread.java
@@ -233,6 +233,7 @@ public class HttpRequestThread extends Thread {
     String line;
     while ((line = reader.readLine()) != null) {
       bufferedLines.add(line);
+      System.out.println("buffer");
       System.out.println(line);
 
       if (line.equals("\n") || line.equals("\r\n")) {
